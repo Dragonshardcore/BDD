be1fe86b0454e62732c4aa54bd3369ec9e4d6a1b
Carlos Xavier Campos Godoy 4 IFM <carlos.campos@uisek.edu.ec>
2026-07-17 18:16:24 -0500

    Update CRUD PARA EL VIEWSET mongo.py

--- Backend/libros/mongo.py
+++ Backend/libros/mongo.py
@@ -1,152 +1,65 @@
 from pymongo import MongoClient
 from bson import ObjectId
 
-client = MongoClient("mongodb://localhost:27017/")
+
+client = MongoClient(
+    "mongodb://localhost:27017/"
+)
 
 db = client["biblioteca_distribuida"]
 
-libros_collection = db["libros"]
-autores_collection = db["autores"]
-reservas_collection = db["reservas"]
-logs_collection = db["logs_sincronizacion"]
 
-autores_collection.create_index(
-    "id_sql",
-    unique=True,
-    sparse=True
-)
+class MongoCRUD:
 
-libros_collection.create_index(
-    "id_sql",
-    unique=True,
-    sparse=True
-)
-reservas_collection.create_index(
-    "id_sql",
-    unique=True,
-    sparse=True
-)
-
-# ============================
-# CREATE
-# ============================
-
-def crear_libro(documento):
-
-    resultado = libros_collection.insert_one(documento)
-
-    return str(resultado.inserted_id)
+    def __init__(self, collection):
+        self.collection = db[collection]
 
 
-# ============================
-# READ
-# ============================
+    def create(self, data):
 
-def obtener_libros():
+        resultado = self.collection.insert_one(data)
 
-    libros = []
-
-    for libro in libros_collection.find():
-
-        libro["_id"] = str(libro["_id"])
-
-        libros.append(libro)
-
-    return libros
+        return str(resultado.inserted_id)
 
 
-# ============================
-# UPDATE
-# ============================
+    def get_all(self):
 
-def actualizar_libro(id, datos):
+        datos = []
 
-    libros_collection.update_one(
+        for item in self.collection.find():
 
-        {"_id": ObjectId(id)},
+            item["_id"] = str(item["_id"])
 
-        {"$set": datos}
+            datos.append(item)
 
-    )
+        return datos
 
 
-# ============================
-# DELETE
-# ============================
+    def update(self, id, data):
 
-def eliminar_libro(id):
-
-    libros_collection.delete_one(
-
-        {"_id": ObjectId(id)}
-
-    )
-# ============================
-# AUTORES
-# ============================
-
-def crear_autor(documento):
-    resultado = autores_collection.insert_one(documento)
-    return str(resultado.inserted_id)
+        self.collection.update_one(
+            {
+                "_id": ObjectId(id)
+            },
+            {
+                "$set": data
+            }
+        )
 
 
-def obtener_autores():
+    def delete(self, id):
 
-    autores = []
-
-    for autor in autores_collection.find():
-        autor["_id"] = str(autor["_id"])
-        autores.append(autor)
-
-    return autores
+        self.collection.delete_one(
+            {
+                "_id": ObjectId(id)
+            }
+        )
 
 
-def actualizar_autor(id, datos):
+# Colecciones
 
-    autores_collection.update_one(
-        {"_id": ObjectId(id)},
-        {"$set": datos}
-    )
+autores_mongo = MongoCRUD("autores")
 
+libros_mongo = MongoCRUD("libros")
 
-def eliminar_autor(id):
-
-    autores_collection.delete_one(
-        {"_id": ObjectId(id)}
-    )
-
-# ============================
-# RESERVAS
-# ============================
-
-def crear_reserva(documento):
-
-    resultado = reservas_collection.insert_one(documento)
-
-    return str(resultado.inserted_id)
-
-
-def obtener_reservas():
-
-    reservas = []
-
-    for reserva in reservas_collection.find():
-        reserva["_id"] = str(reserva["_id"])
-        reservas.append(reserva)
-
-    return reservas
-
-
-def actualizar_reserva(id, datos):
-
-    reservas_collection.update_one(
-        {"_id": ObjectId(id)},
-        {"$set": datos}
-    )
-
-
-def eliminar_reserva(id):
-
-    reservas_collection.delete_one(
-        {"_id": ObjectId(id)}
-    )
+reservas_mongo = MongoCRUD("reservas")
